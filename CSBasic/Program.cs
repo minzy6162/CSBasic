c9a2ef16313e6ea60fbaf57fbe5e3ad792ffcef8
MinJung Choi <s2019s37@e-mirim.hs.kr>
2021-04-19 15:56:26 +0900

    #1 자료형 GetType()

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -89,6 +89,25 @@ namespace CSBasic
             Console.WriteLine(number--);    //11   12
             Console.WriteLine(--number);    //10   10
             Console.WriteLine(number);      //10
+
+
+            //자료형 검사
+            int _int = 273;
+            long _long = 65132165132151321;
+            float _float = 52.273F;
+            double _double = 52.273;
+            char _char = '글';
+            string _string = "문자열";
+
+            Console.WriteLine(_int.GetType());
+            Console.WriteLine(_long.GetType());
+            Console.WriteLine(_float.GetType());
+            Console.WriteLine(_double.GetType());
+            Console.WriteLine(_char.GetType());
+            Console.WriteLine(_string.GetType());
+            Console.WriteLine((2211111111L).GetType());
+
+
         }
     }
 }
