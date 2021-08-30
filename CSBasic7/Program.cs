24131629ed6e0067906aad5d4d30f544aba2eafc
user <user@LAPTOP-B2VSR0FD>
2021-08-30 14:47:55 +0900

    커밋

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace CSBasic7
 {
+    class Wanted<T>
+    {
+        public T Value;
+
+        public Wanted(T value)
+        {
+            this.Value = value;
+        }
+    }
+    class Student
+    {
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("스프링");
+            Wanted<int> wantedInt = new Wanted<int>(3);
+            Wanted<Student> wantedStudent = new Wanted<Student>(new Student());
         }
     }
 }
