26942a53f7692bea94aa03efcc85c30d12970a7a
user <user@LAPTOP-B2VSR0FD>
2021-08-30 15:12:31 +0900

    out 키워드 예제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace CSBasic7
 {
+    class Products
+    {
+        private List<string> products = new List<string>();
+        public Products()
+        {
+            products.Add("짜장면");
+            products.Add("짬뽕");
+            products.Add("탕수육");
+            products.Add("마파두부");
+            products.Add("나가사키짬뽕");
+            products.Add("꽃빵");
+        }
+        public string this[int i]
+        {
+            get
+            {
+                return products[i];
+            }
+            set { Console.WriteLine(i + "번째 상품 설정"); }
+        }
+    }
     class Wanted<T>
+    //where T : Student
     {
         public T Value;
-
         public Wanted(T value)
         {
             this.Value = value;
@@ -21,9 +42,41 @@ namespace CSBasic7
     }
     class Program
     {
+        static void NextPosition(int x, int y, int vx, int vy,
+            out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
+        }
+
         static void Main(string[] args)
         {
-            Wanted<string> wantedString = new Wanted<string>("스프링");
+            int x = 0;
+            int y = 0;
+            int vx = 100;
+            int vy = 100;
+            Console.WriteLine("현재 좌표: (" + x + ", " + y + ")");
+            NextPosition(x, y, vx, vy, out x, out y);
+            Console.WriteLine("다음 좌표: (" + x + ", " + y + ")");
+
+            Console.WriteLine("숫자 입력: ");
+            int output = int.MinValue;
+            bool result = int.TryParse(Console.ReadLine(), out output);
+            if (result)
+            {
+                Console.WriteLine("입력한 숫자:" + output);
+            }
+            else
+            {
+                Console.WriteLine("숫자를 입력해주세요!" + output);
+            }
+
+
+            Products p = new Products();
+            Console.WriteLine(p[2]);
+            Console.WriteLine((new Products())[2]);
+
+            Wanted<string> wantedString = new Wanted<string>("스트링");
             Wanted<int> wantedInt = new Wanted<int>(3);
             Wanted<Student> wantedStudent = new Wanted<Student>(new Student());
         }
